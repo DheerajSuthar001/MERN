ce7c3475cbea81a42f27f1e043eb1cda1ba4649e
DheerajSuthar001 <dheerajsuthar.it21@gmail.com>
2024-09-22 15:46:30 +0530

    Learnr basics - dynamic typing,operators etc.

---
+++ JSClass1.js
@@ -0,0 +1,45 @@
+console.log("NaMASTE dUNIYA");
+// {
+//     let a=23 ;
+// }
+
+// let a=6;
+// var a=12;
+// var a=34;
+// var b=6;
+// {
+//    b=10;
+// }
+// console.log(b);
+// let a=7.6;
+// a='dheeraj';
+// console.log(a);
+
+// a=7896;
+// console.log(a);
+
+// let o={
+//     name:"Hritik",
+//     age:27
+// };
+// console.log(o["name"]);
+
+// let a =[2,"dheeraj",1,6,8];
+// console.log(a[1]);
+// let a=2;
+// console.log(2**4);
+// let a=5;
+// let b='5';
+// console.log(a==b);
+// console.log(a===b);
+
+// console.log(false && true );
+
+for(let i=1;i<5;i++){
+    console.log('dheeraj');
+}
+let i=0;
+while(i<5){
+    console.log('dheeraj'+ ++i);
+    
+}
